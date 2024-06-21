69fe6f759906054a364013d929ac5567fa19c2a5
Angad Singh <mail@angad.dev>
2024-06-21 22:58:01 +0530

    feat: replace tiptap with lexical

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,12 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "plugin:@typescript-eslint/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react/prop-types': 'off',
--- src/lib/ext/notes/EditPage.jsx
+++ src/lib/ext/notes/EditPage.jsx
@@ -1,38 +1,45 @@
 import { Box, Flex, Text, Spinner, Input, useToast } from "@chakra-ui/react";
-import { useState, useEffect, useContext } from "react";
-import { EditorContent, useEditor } from '@tiptap/react';
+import { useState, useContext, useEffect } from "react";
 import * as _ from "lodash";
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import TiptapText from '@tiptap/extension-text'
+import { $getRoot, $getSelection } from 'lexical';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AuthContext } from '../../authcontext';
 
 import "./editor.css";
 
+
+const theme = {
+  // Theme styling goes here
+  //...
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error) {
+  console.error(error);
+}
+
 export function EditPage({ document }) {
+  const [title, setTitle] = useState(document.content.title);
+  const [editorState, setEditorState] = useState()
   const { token } = useContext(AuthContext);
   const queryClient = useQueryClient();
   const toast = useToast();
-  const [title, setTitle] = useState(document.content.title);
-  const editor = useEditor({
-    extensions: [
-      Document,
-      Paragraph,
-      TiptapText,
-    ],
-    content: document.content.content,
-    // onUpdate: ({ editor }) => {
-    //   console.log(JSON.stringify(editor.getJSON(), null, 2));
-    // }
-  });
 
 
   const updateM = useMutation(
     {
       mutationFn: async (content) => {
-        const currentContent = { title, content };
-        if (_.isEqual(currentContent, document.content)) {
+        if (_.isEqual({ title, content }, document.content)) {
           return { success: true, document };
         }
 
@@ -69,15 +76,6 @@ export function EditPage({ document }) {
     }
   );
 
-  useEffect(() => {
-    // console.log(editor.getJSON());
-    const updateInterval = setInterval(() => {
-      updateM.mutate(editor.getJSON());
-    }, 1000);
-
-    return () => clearInterval(updateInterval);
-  }, [editor]);
-
   return <Box>
     <Flex alignItems="center" justifyContent={"space-between"}>
       <Text
@@ -96,17 +94,20 @@ export function EditPage({ document }) {
           <Text ml={2}>Saving...</Text>
         </Flex>
       ) : (
-        <Flex alignItems="center" onClick={() => updateM.mutate(editor?.getJSON())} cursor="pointer">
+        <Flex
+          alignItems="center"
+          onClick={() => updateM.mutate(editor?.getJSON())}
+          cursor="pointer">
           <Box
             width="10px"
             height="10px"
             borderRadius="50%"
-            bg={_.isEqual(document.content, { title, content: editor?.getJSON() })
+            bg={_.isEqual(document.content, { title, content: editorState })
               ? "green.500"
               : "red.500"}
           />
           <Text ml={2}>
-            {_.isEqual(document.content, { title, content: editor?.getJSON() })
+            {_.isEqual(document.content, { title, content: editorState })
               ? "Saved"
               : "Unsaved Changes"}
           </Text>
@@ -129,11 +130,25 @@ export function EditPage({ document }) {
         mt={0}
         mb={4}
       />
-      <Box className="tiptap">
-        <EditorContent
-          editor={editor}
-        />
+      <Box className="lexical">
+        <LexicalComposer initialConfig={{
+          namespace: 'parcha-note',
+          theme,
+          onError,
+        }}>
+          <RichTextPlugin
+            contentEditable={<ContentEditable />}
+            placeholder={<div>Enter some text...</div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <OnChangePlugin onChange={(es) => {
+            console.log(es);
+            setEditorState(es);
+          }} />
+        </LexicalComposer>
       </Box>
     </Box>
-  </Box>
+  </Box >
 }
